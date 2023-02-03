8b147ce5f6a53ba93360a823b5e8fc24e6e21c2a
chesterparker3201 <Chester.j.parker@gmail.com>
2023-02-02 20:01:04 -0500

    Working on Limelight 2/2/23

--- src/main/java/frc/robot/subsystems/Positioning.java
+++ src/main/java/frc/robot/subsystems/Positioning.java
@@ -6,6 +6,12 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.DoubleArraySubscriber;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -13,18 +19,34 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Positioning extends SubsystemBase {
   private NetworkTable m_limelight = null;
+  private DoubleArraySubscriber camtransub;
+  private DoubleArraySubscriber posesub;
+  private Translation2d tran2d;
+  private Rotation2d r2d;
+  private Pose2d p2d;
+  private NetworkTable m_table;
+  private String m_tableName;
 
   NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
   int tv = m_limelight.getEntry("tv").getNumber(0).intValue();
   int botpose_wpiblue = m_limelight.getEntry("botpose_wpiblue").getNumber(0).intValue();
+
+  
   
  
   Pose2d m_botpose = null;
+
+  
 
 
 
   /** Creates a new Positioning. */
   public Positioning() {
+
+    m_tableName = "limelight";
+    m_table = NetworkTableInstance.getDefault().getTable(m_tableName);
+    posesub = m_table.getDoubleArrayTopic("botpose").subscribe(new double[] {});
+
     m_limelight = NetworkTableInstance.getDefault().getTable("limelight-rambot");
 
     
@@ -36,6 +58,18 @@ public class Positioning extends SubsystemBase {
 
   }
 
+  public Pose2d getRobotPose() {
+
+    System.out.println("Geting Robot Pose");
+
+    double[] result = posesub.get();
+    tran2d = new Translation2d(result[0], result[1]);
+    r2d = new Rotation2d(result[3]);
+    p2d = new Pose2d(tran2d, r2d);
+
+    return p2d;
+}
+
   public void updateVision(SwerveDrivePoseEstimator odometry) {
 
 
@@ -45,10 +79,8 @@ public class Positioning extends SubsystemBase {
     // Pose2d botpose = new Pose2d(bluepose[0], bluepose[1], null);
     
     if (tv == 1) {
+      odometry.addVisionMeasurement(getRobotPose(), 1);
       
-      
-      odometry.addVisionMeasurement(m_botpose, 1);
-
     }
   }
 
@@ -59,7 +91,7 @@ public class Positioning extends SubsystemBase {
         // check that we have a valid potpose
         if (tv == 1) {
 
-          DrivetrainSubsystem.getOdometry().addVisionMeasurement(m_botpose, 1);
+          DrivetrainSubsystem.getOdometry().addVisionMeasurement(getRobotPose(), 1);
         }
 
         // get the odometry from drive train
